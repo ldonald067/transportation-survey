74f21a60e6e1a75f6d74c48162ea99d75e957090
LaTanya Donaldson <ldonald234@gmail.com>
2017-07-07 15:46:21 -0700

    set the values of checked boxes aside in an array:

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,11 @@ $(document).ready(function(){
   $("form#transportation_survey").submit(function(event){
     event.preventDefault();
     $("#work-responses").show();
+     var userResponses = [];
     $("input:checkbox[name=work-transportation]:checked").each(function(){
       var workTransportationMode = $(this).val();
       var capitalWorkTransportationMode = workTransportationMode.toUpperCase();
+      userResponses.push(capitalWorkTransportationMode);
       $('#work-responses').append(capitalWorkTransportationMode + "<br>");
     });
     $("#fun-responses").show();
